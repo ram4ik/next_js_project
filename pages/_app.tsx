c9f601a06f0b4424ce6ce53620c4a739a2671676
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-12-19 21:00:30 +0200

    using useEffect

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import App from 'next/app';
+import { useEffect } from 'react';
 import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { ThemeProvider } from '@material-ui/core/styles';
@@ -6,6 +6,13 @@ import { ThemeProvider } from '@material-ui/core/styles';
 import { themeDark, themeLight } from 'lib/theme';
 
 export default function MyApp({ Component, pageProps}) {
+    useEffect(() => {
+        const jsStyles = document.querySelector('#jss-server-side');
+        if (jsStyles && jsStyles.parentNode) {
+            jsStyles.parentNode.removeChild(jsStyles);
+        }
+    }, [])
+
     return ( 
         <ThemeProvider theme={false ? themeDark : themeLight}>
             <CssBaseline />
