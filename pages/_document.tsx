9b7740368994b2377f29d4fb9d878a3beb0758f8
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-12-19 21:13:13 +0200

    Server Stylesheets

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,23 @@
 import Document, { DocumentContext, DocumentInitialProps, Head, Html, Main, NextScript } from "next/document";
+import { ServerStyleSheets} from '@material-ui/core';
+import React from "react";
 
 class MyDocument extends Document {
 
     static async getInitialProps(ctx) {
+        const sheets = new ServerStyleSheets();
+        const originalRenderPage = ctx.renderPage;
+
+        ctx.renderPage = () => originalRenderPage({ 
+            enhancedApp: (App) => (props) => sheets.collect(<App {...props} />),
+        });
+        
         const initialProps = await Document.getInitialProps(ctx);
-        return {...initialProps };
+        return {...initialProps, 
+            styles: [ 
+                ...React.Children.toArray(initialProps.styles), sheets.getStyleElement(),
+            ],
+        };
     }
 
     render() {
