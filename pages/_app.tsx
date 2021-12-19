71333753cf90e4af8d27f105115ce6d4bfd31703
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-12-19 20:55:32 +0200

    Theme provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
 import App from 'next/app';
 import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/core/styles';
 
-export default ({ Component, pageProps}) => (
-    <Component {...pageProps} />
-)
+import { themeDark, themeLight } from 'lib/theme';
+
+export default function MyApp({ Component, pageProps}) {
+    return ( 
+        <ThemeProvider theme={false ? themeDark : themeLight}>
+            <CssBaseline />
+            <Component {...pageProps} /> 
+        </ThemeProvider>
+        
+    )
+}
